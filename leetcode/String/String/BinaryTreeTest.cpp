39416198eb8d45a3f7be43860c266f1af05c23af
limaofuyuanzhang <397524331@qq.com>
2016-08-08 18:01:41 +0800

    2016-08-08 company

--- leetcode/String/String/BinaryTreeTest.cpp
+++ leetcode/String/String/BinaryTreeTest.cpp
@@ -6,31 +6,33 @@
 //  Copyright © 2016年 limaofuyuanzhang. All rights reserved.
 //
 
-#include "BinaryTreeTest.hpp"
+#include <stdio.h>
+#include "gtest/gtest.h"
+#include "TreeNode.hpp"
+#include "BinaryTree.hpp"
+#include <sstream>
+using namespace std;
 
-
-void testMaxDepth(){
-    printf("test maxDepth begin--------------------------------------------\n");
+TEST(BinaryTreeTest, maxDepth){
     TreeNode *nullTree = deserialize("{}");
     int depth1 = maxDepth(nullTree);
-    LTTest(0, depth1);
+    EXPECT_EQ(0,depth1);
     
     TreeNode *oneTree = deserialize("{1}");
     int depth2 = maxDepth(oneTree);
-    LTTest(1, depth2);
+    EXPECT_EQ(1, depth2);
     
     TreeNode *twoTree = deserialize("{1,2,3}");
     int depth3 = maxDepth(twoTree);
-    LTTest(2, depth3);
+    EXPECT_EQ(2, depth3);
     
     TreeNode *leftTree = deserialize
     ("{1,2,#,3,#,4}");
     int depth4 = maxDepth(leftTree);
-    LTTest(4, depth4);
+    EXPECT_EQ(4, depth4);
     
     TreeNode *rightTree = deserialize("{1,#,2,#,3,#,4}");
     int depth5 = maxDepth(rightTree);
-    LTTest(4, depth5);
-    
-    printf("test maxDepth end----------------------------------------------\n");
+    EXPECT_EQ(4, depth5);
+
 }
