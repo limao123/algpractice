f3979f85e0d588c756fc4475bd54cb2c429bf683
limaofuyuanzhang <397524331@qq.com>
2016-10-26 22:28:21 +0800

    no message

--- Practise/Practise/main.cpp
+++ Practise/Practise/main.cpp
@@ -8,8 +8,24 @@
 
 #include <iostream>
 
+int func(int n,int m) {
+    if (n<= 0 || m <= 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return m;
+    }
+    if (m==1) {
+        return n;
+    }
+    return func(n-1,m)+func(n, m-1);
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int b;
+    std::cin >> b;
+    int a = func(4, 5);
+    std::cout << a;
     return 0;
 }
